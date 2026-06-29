cf359a06fe6af1b8643a7050133ae23d9818ccf0
Vaishali Singh <vaishalisingh983927@gmail.com>
2026-06-29 13:56:08 +0530

    fix CORS for Vercel

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post(
 */
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://prep-pilot-ai-wz5b.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
