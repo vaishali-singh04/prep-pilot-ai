9bc0c0654c1348291e180a8a5f8e1dade0f4a766
Vaishali Singh <vaishalisingh983927@gmail.com>
2026-06-29 13:17:46 +0530

    fix firebase API key env name

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "examnotesai-4b733.firebaseapp.com",
   projectId: "examnotesai-4b733",
   storageBucket: "examnotesai-4b733.firebasestorage.app",
